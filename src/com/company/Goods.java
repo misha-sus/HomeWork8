4dcf9e3e7e2305503080ad67b56eea5aacbc3bb2
Misha Khush <Misha26099777@yandex.ru>
2021-08-20 13:51:41 +0300

    В классе Main создал список багажей и отсортировал по весу.

--- src/com/company/Goods.java
+++ src/com/company/Goods.java
@@ -1,7 +1,43 @@
 package com.company;
 
 public class Goods {
-    String nameCarrier;
-    String nameFlight;
-    double maxWeight;
+    private String nameCarrier;
+    private String nameFlight;
+    private double maxWeight;
+
+    public Goods(String nameCarrier, String nameFlight, double maxWeight) {
+        this.nameCarrier = nameCarrier;
+        this.nameFlight = nameFlight;
+        this.maxWeight = maxWeight;
+    }
+
+    public String getNameCarrier() {
+        return nameCarrier;
+    }
+
+    public void setNameCarrier(String nameCarrier) {
+        this.nameCarrier = nameCarrier;
+    }
+
+    public String getNameFlight() {
+        return nameFlight;
+    }
+
+    public void setNameFlight(String nameFlight) {
+        this.nameFlight = nameFlight;
+    }
+
+    public double getMaxWeight() {
+        return maxWeight;
+    }
+
+    public void setMaxWeight(double maxWeight) {
+        this.maxWeight = maxWeight;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Авиаперевозчик %s, класс -%s ,вес багажа - %s" ,nameCarrier,nameFlight,maxWeight);
+    }
+
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	  List<Goods> goodsList = new ArrayList<>();
+	  goodsList.add(new Goods("Аэрофлот","эконом",20));
+	  goodsList.add(new Goods("Ред вингс","бизнес",13));
+	  goodsList.add(new Goods("Аэрофлот","эконом",23));
+	  goodsList.add(new Goods("S7","комфорт",18));
+	  goodsList.add(new Goods("Победа","первый",40));
+		goodsList.sort((o1,o2)-> o1.getMaxWeight() > o2.getMaxWeight()?1:-1);
+	  for(Goods goods: goodsList){
+          System.out.println(goods);
+      }
     }
 }
